978ab5acd96cc1e56b00e2ebd663fccedaa9ec30
kimnle <kimnle002@gmail.com>
2024-04-06 13:06:42 +1100

    ternary operator

---
+++ ternary_operator.py
@@ -0,0 +1,10 @@
+is_raining = input("Is it raining? Yes or no? ")
+# if is_raining:
+#     print("I need an umbrella")
+# else:
+#     print("I don't need an umbrella")
+
+# Ternary operator
+# <statement to execute when true> if <condition> else <statement to execute when false>
+
+print("You need an umbrella") if is_raining == "Yes" else print("You don't need an umbrella")
